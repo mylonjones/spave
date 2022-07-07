a2b6b730d59aadf39c45ba29c3bdb6543533fbb0
mylonjones <mylonndj1234@gmail.com>
2022-07-07 16:25:24 -0700

    added process.env.port to server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000
+const port = 3000
 const path = require('path');
 
 const cloudinary = require('cloudinary')
